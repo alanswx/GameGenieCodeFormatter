9057e81cb65f044448aac2a89a3f59ddbcea0028
Alan Steremberg <alan.steremberg@gmail.com>
2019-04-25 21:20:13 -0700

    comments

--- index.js
+++ index.js
@@ -2,17 +2,23 @@
 //var result = decodeNES("ANTOSA");
 //console.log(code);
 //console.log(result);
+
+// bring in the ggencoder library that we changed to have node exports
 var gg = require("./ggencoder");
-console.log("hello");
 
 const csv = require('csv-parser');  
 const fs = require('fs');
 
+// read the csv we created from the master spreadsheet. This 
+// is hardcoded to NES for now.
 fs.createReadStream('nes.csv')  
   .pipe(csv())
   .on('data', (row) => {
+
+   // temporarily filter just Super Mario Bros. 
    if (row["Game"]=="Super Mario Bros.")
    {
+      // some rows are blank, or just have comments. Usually the Effect field is blank
       if (row["Effect"].length<=0)
       {  
 	console.log("skipping:");
@@ -22,47 +28,47 @@ fs.createReadStream('nes.csv')
     console.log(row);
 
     /*split the key into pieces, and create an array */
+    /* some effects have multiple game genie keys to make them work, separated by + in the spreadsheet */
     var keys = row["Key"].split("+");
     console.log(keys);
     var len;
+    // dar is an array of Uint32 arrays 4 bytes large
     var dar=new Array();
     for (var i = 0, len = keys.length; i < len; i++) 
     {
+       // we need to trim the space from before and after the + if there were any spaces
        var key = keys[i].trim();
-       console.log(i);
-       console.log(key);
-       console.log(keys.length);
+       // decode the key
        var result=gg.gg_decodeNES(key);
     	console.log(result);
-//1'b enable, 1'b compare, 000000 , address, compare, replace
-//RawCode { value: 243, address: 12344, compare: 65 }
+        // the result looks like this:
+        //RawCode { value: 243, address: 12344, compare: 65 }
         var data = new Uint32Array(4);
+        //1'b enable, 1'b compare, 000000 , address, compare, replace
     	data[0]=0;
     	data[1]=result.address;
 	data[2]=result.compare;
 	data[3]=result.value;
         dar.push(data);
-    	console.log(data);
-    	for (var j=0;j<data.length;j++)
-		console.log(data[j].toString(16).padStart(8,'0'));
-
     }
-    	var dir = "nesgg/" + row["Game"][0]+"/"+  row["Game"];
-    	console.log("dir:"+dir);
-    	fs.mkdirSync(dir, { recursive: true });
-    	var file= dir +"/"+ row["Effect"]+".gg";
-    	console.log(dar);
-        var outdata = new Uint32Array(4*dar.length);
-    	for (var j=0;j<dar.length;j++) 
-	{
-          console.log(dar[j]);
+    // create the directory tree
+    var dir = "nesgg/" + row["Game"][0]+"/"+  row["Game"];
+    console.log("dir:"+dir);
+    fs.mkdirSync(dir, { recursive: true });
+    var file= dir +"/"+ row["Effect"]+".gg";
+    //console.log(dar);
+    var outdata = new Uint32Array(4*dar.length);
+    for (var j=0;j<dar.length;j++) 
+    {
+          //console.log(dar[j]);
           outdata[j*4]=dar[j][0];
           outdata[j*4+1]=dar[j][1];
           outdata[j*4+2]=dar[j][2];
           outdata[j*4+3]=dar[j][3];
-	}
-          console.log(outdata);
-    	fs.writeFile( file, outdata, 'binary', function(err){ if (err) console.log('err:'+err)});
+    }
+    console.log(outdata);
+    // make the binary file
+    fs.writeFile( file, outdata, 'binary', function(err){ if (err) console.log('err:'+err)});
    }
   }
 
